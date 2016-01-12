dba0414b768f7d60493f3a61c116ad8e6e9f27f2
Benjamin Roedell <benrobot@aol.com>
2016-01-11 23:59:28 -0500

    Display teapot ASCII art on view as part of following https://devcenter.heroku.com/articles/getting-started-with-python#push-local-changes

--- CoreActivityTracker/views.py
+++ CoreActivityTracker/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the CoreActivityTracker index.")
+    r = requests.get('http://httpbin.org/status/418')
+    print r.text
+    return HttpResponse('<pre>' + r.text + '</pre>')
