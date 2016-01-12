4a1e810b023b77a19d400b1800e318f8ae9d0786
Benjamin Roedell <benrobot@aol.com>
2016-01-12 00:18:44 -0500

    Learning to set configurations as part of following https://devcenter.heroku.com/articles/getting-started-with-python#define-config-vars

--- CoreActivityTracker/views.py
+++ CoreActivityTracker/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import os
 
 
 def index(request):
-    r = requests.get('http://httpbin.org/status/418')
-    print r.text
-    return HttpResponse('<pre>' + r.text + '</pre>')
+    times = int(os.environ.get('TIMES',3))
+    return HttpResponse('Hello!' * times)
