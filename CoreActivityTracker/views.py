b0d66fe19627b3f9f6dfcad3670785e9bf14555d
Benjamin Roedell <benrobot@aol.com>
2016-02-01 21:08:51 -0500

    Add login_required decorator to view controller definition.  This ensures that user has already been authenticated before viewing this page.  Only problem is that the accounts app and views have not been created so there is currently no way to arrive at the intended page until a login page is created.

--- CoreActivityTracker/views.py
+++ CoreActivityTracker/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 import requests
 import os
+from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def index(request):
     times = int(os.environ.get('TIMES',3))
     return HttpResponse('Hello!' * times)
