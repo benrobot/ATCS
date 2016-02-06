edb4f9811086b922885cd4263abe6c5b808ec226
Benjamin Roedell <benrobot@aol.com>
2016-02-06 11:32:41 -0500

    Fixed deprecation warning "RemovedInDjango110Warning: Support for string view arguments to url() is deprecated and will be removed in Django 1.10. Pass callable instead."

--- ATCS/urls.py
+++ ATCS/urls.py
@@ -16,11 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from login.views import *
+from django.contrib.auth import views as django_auth_views
 
 urlpatterns = [
-    url(r'^$', django.contrib.auth.views.login),
+    url(r'^$', django_auth_views.login),
     url(r'^logout/$', logout_page),
-    url(r'^accounts/login/$', django.contrib.auth.views.login), # If user is not login it will redirect to login page
+    url(r'^accounts/login/$', django_auth_views.login), # If user is not login it will redirect to login page
     url(r'^register/$', register),
     url(r'^register/success/$', register_success),
     url(r'^home/$', home),
--- CoreActivityTracker/urls.py
+++ CoreActivityTracker/urls.py
@@ -1,8 +1,8 @@
 from django.conf.urls import url
-
+from django.contrib.auth import views as django_auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^login/$', 'django.contrib.auth.views.login', name="my_login"),
+    url(r'^login/$', django_auth_views.login, name="my_login"),
 ]
