b57a6810d1acf8020a4c773a91cecffa6fee199e
Benjamin Roedell <benrobot@aol.com>
2016-02-06 11:01:06 -0500

    Remove login/urls.py and move redirects into projects urls.py file

--- ATCS/urls.py
+++ ATCS/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from login.views import *
 
 urlpatterns = [
+    url(r'^$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', logout_page),
+    url(r'^accounts/login/$', 'django.contrib.auth.views.login'), # If user is not login it will redirect to login page
+    url(r'^register/$', register),
+    url(r'^register/success/$', register_success),
+    url(r'^home/$', home),
     url(r'^(?i)CoreActivityTracker/', include('CoreActivityTracker.urls')),
-    url(r'^login/', include('login.urls')),
     url(r'^admin/', admin.site.urls),
 ]
