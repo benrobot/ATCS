e84ee41d275b36ddb45e209846354fca4e62590c
Benjamin Roedell <benrobot@aol.com>
2016-09-22 21:11:08 -0400

    Changing to psycopg2 2.6.2 (instead of 2.6.1) and bringing in SSLRedirectMiddleware to always forward to SSL version of a page.

--- ATCS/settings.py
+++ ATCS/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE_CLASSES = [
+    'ssl_redirect.middleware.SSLRedirectMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
