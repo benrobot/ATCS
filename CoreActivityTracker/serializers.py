3e61fe5714eb02f3295dcee88e66ea9bcd07a2a5
Benjamin Roedell <benrobot@aol.com>
2016-10-01 10:18:14 -0400

    Add 'id' field to SpokenLanguage serializer (without it how can we edit existing records)

--- CoreActivityTracker/serializers.py
+++ CoreActivityTracker/serializers.py
@@ -5,4 +5,4 @@ from rest_framework import serializers
 class SpokenLanguageSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = SpokenLanguage
-        fields = ('english_name', 'iso_639_2_code', 'iso_639_1_code', 'biblio_or_term')
+        fields = ('id', 'english_name', 'iso_639_2_code', 'iso_639_1_code', 'biblio_or_term')
