61335ad95e45c5000d8e11b43fc6ffda106f2585
Benjamin Roedell <benrobot@aol.com>
2016-10-08 08:26:53 -0400

    Explicitly defining API views

--- CoreActivityTracker/urls.py
+++ CoreActivityTracker/urls.py
@@ -1,21 +1,12 @@
 from django.conf.urls import url, include
 from django.contrib.auth import views as django_auth_views
-from rest_framework import routers, viewsets
-from . import views, serializers, models
-
-
-# ViewSets define the view behavior.
-class SpokenLanguageViewSet(viewsets.ModelViewSet):
-    queryset = models.SpokenLanguage.objects.all()
-    serializer_class = serializers.SpokenLanguageSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'SpokenLanguages', SpokenLanguageViewSet)
+from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^login/$', django_auth_views.login, name="my_login"),
-    url(r'^api/', include(router.urls)),
-    url(r'^apidoc/', include('rest_framework.urls', namespace='rest_framework'))
+
+    #api
+    url(r'^api/v1/spokenlanguages/$', 'spoken_language_collection'),
+    url(r'^api/v1/spokenlanguages/(?P<pk>[0-9]+)$', 'spoken_language_element')
 ]
--- CoreActivityTracker/views.py
+++ CoreActivityTracker/views.py
@@ -1,16 +1,42 @@
 from django.template.loader import get_template
 from django.template import Context
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from django.http import HttpResponse
 import requests
 import os
 from django.contrib.auth.decorators import login_required
 
+from CoreActivityTracker.models import SpokenLanguage
+from CoreActivityTracker.serializers import SpokenLanguageSerializer
+
+
 @login_required
 def index(request):
     template = get_template('CoreActivityTracker/index.html')
-    times = int(os.environ.get('TIMES',3))
+    times = int(os.environ.get('TIMES', 3))
     context = {
         'range_of_times_to_repeat': range(times),
     }
     return HttpResponse(template.render(context, request))
+
+
+@api_view(['GET'])
+def spoken_language_collection(request):
+    if request.method == 'GET':
+        objects = SpokenLanguage.objects.all()
+        serializer = SpokenLanguageSerializer(objects, many=True)
+        return Response(serializer.data)
+
+
+@api_view(['GET'])
+def spoken_language_element(request, pk):
+    try:
+        objects = SpokenLanguage.objects.get(pk=pk)
+    except SpokenLanguage.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = SpokenLanguageSerializer(objects)
+        return Response(serializer.data)
